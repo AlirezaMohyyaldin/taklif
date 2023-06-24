4069b352993adccefd6bae5a3d8636880644fff1
Alireza.M.H.N <126543376+AlirezaMohyyaldin@users.noreply.github.com>
2023-06-24 21:13:19 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x  = Convert.ToDouble(Console.ReadLine());
+            calc(x);
+            
+            
+        }
+        private static void calc(double x)
+        {
+            double resault = Math.Pow(x, 2) + (x * 2) - 4;
+            Console.WriteLine(resault);
+        }
+    }
+}
